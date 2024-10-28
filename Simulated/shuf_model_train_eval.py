3324ddbe8e2d84741fc9cb62d87db9389b37a0ec
Dibyabha <144924291+Dibyabha@users.noreply.github.com>
2024-10-28 16:49:27 +0530

    redistribution of datasets

--- Simulated/shuf_model_train_eval.py
+++ Simulated/shuf_model_train_eval.py
@@ -12,9 +12,10 @@ import os
 import numpy as np
 from netCDF4 import Dataset as NetCDFDataset
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, LeakyReLU, Dropout, BatchNormalization, MaxPool2D, Flatten, Dense, GaussianNoise
+from tensorflow.keras.layers import Input, Conv2D, LeakyReLU, Dropout, BatchNormalization, MaxPool2D, Flatten, Dense, GaussianNoise, Concatenate, ConvTranspose2D, Upsampling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tqdm import tqdm
+from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 import pandas as pd
